d997a1d8896bcab7bb6aec5a272d1a8e040e25de
Riashad Noor <riashad@Riashads-Mac.local>
2017-08-15 02:00:08 +0600

    Initial Commit

---
+++ Basic Text Input & Show Task 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic Text Input & Show Task 1
+//
+//  Created by Riashad Noor on 8/15/17.
+//  Copyright © 2017 Riashad Noor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
