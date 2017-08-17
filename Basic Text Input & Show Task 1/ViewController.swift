16dd32a17fe7c21ab2ea67e1bdb85c1564194ab6
System Administrator <root@Riashads-Mac.local>
2017-08-18 02:32:31 +0600

    Simple Text Input output application. Takes input and shows output on clicking on Submit button

--- Basic Text Input & Show Task 1/ViewController.swift
+++ Basic Text Input & Show Task 1/ViewController.swift
@@ -9,10 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Input Text Field Outlets
+    
+    @IBOutlet weak var fullNameTF: UITextField!
+    
+    @IBOutlet weak var emailTF: UITextField!
+    
+    @IBOutlet weak var universityTF: UITextField!
+    
+    @IBOutlet weak var studentidTF: UITextField!
+    
+    @IBOutlet weak var cellNumTF: UITextField!
+    
+    //Output Labels outles
+    
+    @IBOutlet weak var fullNameLabe: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var universityLabel: UILabel!
+    
+    @IBOutlet weak var studentidLabel: UILabel!
+    
+    @IBOutlet weak var cellNumlabel: UILabel!
+    
+    //Blank Sting Variabls
+    
+    var FullName: String = ""
+    var email: String = ""
+    var University: String = ""
+    var student: String = ""
+    var CellNum: String = ""
+    
+    //Onclick Action Function
+    
+    @IBAction func SubmitOnClick(_ sender: UIButton) {
+        
+        //Getting values from text field
+        FullName = fullNameTF.text!
+        email = emailTF.text!
+        University = universityTF.text!
+        student = studentidTF.text!
+        CellNum = cellNumTF.text!
+        
+        //Outputting Inserted Values
+        fullNameLabe.text = FullName
+        emailLabel.text = email
+        universityLabel.text = University
+        studentidLabel.text = student
+        cellNumlabel.text = CellNum
+        
+        //Emptying the input fields
+        fullNameTF.text = ""
+        emailTF.text = ""
+        universityTF.text = ""
+        studentidTF.text = ""
+        cellNumTF.text = ""
+        
+        //Hiding Keyboard
+        self.view.endEditing(true)
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        fullNameLabe.text = "Please fill up the above forms"
     }
 
     override func didReceiveMemoryWarning() {
